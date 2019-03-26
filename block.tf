37b3b7f5e050e832ff69c9b0d32de6ce43807d1e
tboettjer <tboettjer@outlook.com>
2019-03-26 21:02:55 +0100

    Initial setup

---
+++ block.tf
@@ -0,0 +1,44 @@
+// Copyright (c) 2017, 2019, Oracle and/or its affiliates. All rights reserved.
+
+resource "oci_core_volume" "DCOSBlock" {
+  count               = "${var.NumInstances * var.NumIscsiVolumesPerInstance}"
+  availability_domain = "${data.oci_identity_availability_domain.ad.name}"
+  compartment_id      = "${var.compartment_ocid}"
+  display_name        = "DCOSBlock${count.index}"
+  size_in_gbs         = "${var.DBSize}"
+}
+
+resource "oci_core_volume_attachment" "DCOSBlockAttach" {
+  count           = "${var.NumInstances * var.NumIscsiVolumesPerInstance}"
+  attachment_type = "iscsi"
+  compartment_id  = "${var.compartment_ocid}"
+  instance_id     = "${oci_core_instance.DCOSInstance.*.id[count.index / var.NumIscsiVolumesPerInstance]}"
+  volume_id       = "${oci_core_volume.DCOSBlock.*.id[count.index]}"
+  device          = "${count.index == 0 ? var.volume_attachment_device : ""}"
+
+  # Set this to enable CHAP authentication for an ISCSI volume attachment. The oci_core_volume_attachment resource will
+  # contain the CHAP authentication details via the "chap_secret" and "chap_username" attributes.
+  #use_chap = true
+
+  # Set this to attach the volume as read-only.
+  #is_read_only = true
+}
+
+resource "oci_core_volume" "DCOSBlockParavirtualized" {
+  count               = "${var.NumInstances * var.NumParavirtualizedVolumesPerInstance}"
+  availability_domain = "${data.oci_identity_availability_domain.ad.name}"
+  compartment_id      = "${var.compartment_ocid}"
+  display_name        = "DCOSBlockParavirtualized${count.index}"
+  size_in_gbs         = "${var.DBSize}"
+}
+
+resource "oci_core_volume_attachment" "DCOSBlockAttachParavirtualized" {
+  count           = "${var.NumInstances * var.NumParavirtualizedVolumesPerInstance}"
+  attachment_type = "paravirtualized"
+  compartment_id  = "${var.compartment_ocid}"
+  instance_id     = "${oci_core_instance.DCOSInstance.*.id[count.index / var.NumParavirtualizedVolumesPerInstance]}"
+  volume_id       = "${oci_core_volume.DCOSBlockParavirtualized.*.id[count.index]}"
+
+  # Set this to attach the volume as read-only.
+  #is_read_only = true
+}
---
+++ network.tf
@@ -0,0 +1,38 @@
+// Copyright (c) 2017, 2019, Oracle and/or its affiliates. All rights reserved.
+
+resource "oci_core_virtual_network" "MesosVCN" {
+  cidr_block     = "10.1.0.0/16"
+  compartment_id = "${var.compartment_ocid}"
+  display_name   = "MesosVCN"
+  dns_label      = "Mesosvcn"
+}
+
+resource "oci_core_subnet" "MesosSubnet" {
+  availability_domain = "${data.oci_identity_availability_domain.ad.name}"
+  cidr_block          = "10.1.20.0/24"
+  display_name        = "MesosSubnet"
+  dns_label           = "Mesossubnet"
+  security_list_ids   = ["${oci_core_virtual_network.MesosVCN.default_security_list_id}"]
+  compartment_id      = "${var.compartment_ocid}"
+  vcn_id              = "${oci_core_virtual_network.MesosVCN.id}"
+  route_table_id      = "${oci_core_route_table.MesosRT.id}"
+  dhcp_options_id     = "${oci_core_virtual_network.MesosVCN.default_dhcp_options_id}"
+}
+
+resource "oci_core_internet_gateway" "MesosIG" {
+  compartment_id = "${var.compartment_ocid}"
+  display_name   = "MesosIG"
+  vcn_id         = "${oci_core_virtual_network.MesosVCN.id}"
+}
+
+resource "oci_core_route_table" "MesosRT" {
+  compartment_id = "${var.compartment_ocid}"
+  vcn_id         = "${oci_core_virtual_network.MesosVCN.id}"
+  display_name   = "MesosRouteTable"
+
+  route_rules {
+    destination       = "0.0.0.0/0"
+    destination_type  = "CIDR_BLOCK"
+    network_entity_id = "${oci_core_internet_gateway.MesosIG.id}"
+  }
+}
